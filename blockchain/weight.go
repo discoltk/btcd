0ff82b6e10776664af5edcb373abcccd6854186a
disco <disco@localhost.localdomain>
2017-11-04 15:34:01 +0900

    Basic segwit2x support.  No automatic fork awareness, must bring up manually after fork activates

--- blockchain/weight.go
+++ blockchain/weight.go
@@ -19,16 +19,16 @@ const (
 	// weight of a "base" byte is 4, while the weight of a witness byte is
 	// 1. As a result, for a block to be valid, the BlockWeight MUST be
 	// less than, or equal to MaxBlockWeight.
-	MaxBlockWeight = 4000000
+	MaxBlockWeight = 8000000
 
 	// MaxBlockBaseSize is the maximum number of bytes within a block
 	// which can be allocated to non-witness data.
-	MaxBlockBaseSize = 1000000
+	MaxBlockBaseSize = 2000000
 
 	// MaxBlockSigOpsCost is the maximum number of signature operations
 	// allowed for a block. It is calculated via a weighted algorithm which
 	// weights segragated witness sig ops lower than regular sig ops.
-	MaxBlockSigOpsCost = 80000
+	MaxBlockSigOpsCost = 160000
 
 	// WitnessScaleFactor determines the level of "discount" witness data
 	// receives compared to "base" data. A scale factor of 4, denotes that
